5cdd5f5888d891b3a3239f798ba4821e8cc7da5c
Chetan Kumar <>
2018-11-30 16:04:28 +0530

    ïnitial commit

---
+++ CatalogCollectionView/CatalogCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  CatalogCollectionView.h
+//  CatalogCollectionView
+//
+//  Created by Chetan Kumar on 30/11/18.
+//  Copyright © 2018 chetan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CatalogCollectionView.
+FOUNDATION_EXPORT double CatalogCollectionViewVersionNumber;
+
+//! Project version string for CatalogCollectionView.
+FOUNDATION_EXPORT const unsigned char CatalogCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CatalogCollectionView/PublicHeader.h>
+
+
